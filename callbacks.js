5e87f81939572b7fb9511fbb61d63e4c15821b2d
charan932001 <charan932001@gmail.com>
2025-04-10 17:21:10 +0530

    added callback file

---
+++ callbacks.js
@@ -0,0 +1,22 @@
+function step1(value,callback){
+    callback(value+10,false)    
+}
+function step2(value,callback){
+    callback(value+10,false)    
+}
+function step3(value,callback){
+    callback(value+10,false)    
+}
+step1(0,function(result1,err){
+    if(!err){
+        step2(result1,function(result2,err){
+            if(!err){
+                step3(result2,function(result3,err){
+                    if(!err){
+                        console.log(result3)
+                    }
+                })
+            }
+        })
+    }
+})
